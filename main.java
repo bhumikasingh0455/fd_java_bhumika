7be4610b74fd9b13837b865320c09d5f91eea577
bhumikasingh0455 <bhumikasingh0407@gmail.com>
2025-08-11 10:18:16 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+
+    public static void main(String[] args) {
+       // System.out.println("Hello \nbhumika ");
+       System.out.print("Class");
+       System.out.println("sp");
+    }
+}
